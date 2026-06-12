74c1e1949bf5b543651bf6b3f5794a8ba90d8697
buildwithharish <harishmurali2025@gmail.com>
2026-06-12 11:51:29 +0530

    Change Gemini model to 'gemini-2.0-flash-lite'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 # Gemini setup
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-gemini = genai.GenerativeModel("gemini-2.0-flash")
+gemini = genai.GenerativeModel("gemini-2.0-flash-lite")
 # App title
 st.title("🚦 AI Road Safety Advisor")
 st.write("Fill in the details below to predict accident severity.")
